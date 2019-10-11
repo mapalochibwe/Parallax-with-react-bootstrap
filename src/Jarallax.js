e77365fdeb61cd7c140e3faaa3bb3a34990de727
mapalochibwe <maverickdfh@gmail.com>
2019-10-11 13:18:58 +0200

    Incorporated local files

---
+++ src/Jarallax.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+    serviceWorker.unregister();
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true,
+});
+    return (
+      <div className="App">
+
+    
+      </div>
+    );
+  }
